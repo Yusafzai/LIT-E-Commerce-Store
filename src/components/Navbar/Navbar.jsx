63d9ed65380d50ef8e45263ccff945635649438b
Gefahr Yousafzai <itsyusafzai@gmail.com>
2021-04-18 17:56:36 +0500

    Buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuButton, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/logo.png';
+import { Link, useLocation } from 'react-router-dom';
 import useStyles from './styles';
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
     return (
         <>
           <AppBar position='fixed' className={classes.AppBar} color="inherit">
               <Toolbar>
-                  <Typography variant='h6' className={classes.title} color='inherit'>
+                  <Typography component={Link} to="/" variant='h6' className={classes.title} color='inherit'>
                       <img src={logo} alt="Lit Store" height="25px" className={classes.image} />
                       Lit Store
                   </Typography>
                   <div className={classes.grow} />
+                  {location.pathname == '/' && (
                   <div className={classes.button}>
-                     <IconButton aria-label='Show cart items' color='inherit'>
+                     <IconButton component={Link} to="/cart" aria-label='Show cart items' color='inherit'>
                         <Badge badgeContent={ totalItems } color='secondary'>
                             <ShoppingCart />
                         </Badge>
                      </IconButton>
-                  </div>
+                  </div> )}
               </Toolbar>
          </AppBar>  
         </>
